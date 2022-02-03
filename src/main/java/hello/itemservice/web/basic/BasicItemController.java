4b96e513c645c2e391016a1dcbd620ba0be66bcb
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-04 08:01:39 +0900

    FEAT: 타임리프를 이용하여 상품 상세 페이지 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -28,6 +28,14 @@ public class BasicItemController {
         return "basic/items";
     }
 
+    // 상품 상세
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
     @PostConstruct
     public void init() {
 
