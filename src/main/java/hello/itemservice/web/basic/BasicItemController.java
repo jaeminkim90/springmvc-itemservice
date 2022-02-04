d267ad5f836ebd07c86104114c6820fe0a5133aa
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-04 21:18:04 +0900

    FEAT: 상품 수정 페이지와 컨트롤러 로직 구현

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -110,9 +110,25 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
     @PostConstruct
     public void init() {
-
         itemRepository.save(new Item("ItemA", 10000, 10));
         itemRepository.save(new Item("ItemB", 20000, 20));
         itemRepository.save(new Item("ItemC", 30000, 30));
